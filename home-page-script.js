ec621b783f3b00fdb65e65264454995d0464ab28
AleMag20 <ale00ven@gmail.com>
2024-10-17 14:15:59 +0200

    Primo mio sito finito

---
+++ home-page-script.js
@@ -0,0 +1,3 @@
+alert("benvenuto nel mio sito!");
+var utente=window.prompt ("Conosciamoci! Come ti chiami?");
+document.getElementById("titolo").innerHTML = "Benvenuto " +  utente + ", oggi ti parlerlò di...";
